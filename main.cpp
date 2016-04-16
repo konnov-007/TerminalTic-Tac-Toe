6378dfb609c166ae40411403b64c3b27b973fee9
Mr.Konnov <konnov-007@mail.ru>
2016-04-17 00:33:34 +0600

    Added "about" menu

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 char table[3][3];
 
 
+
 void title()
 {
 	printf("XXXXX  X  XXXXX     XXXXX    X    XXXXX     XXXXX    X    XXXXX\n");
@@ -11,6 +12,16 @@ void title()
 	printf("  X    X  X     XXX   X    XXXXX  X     XXX   X    X   X  X    \n");
 	printf("  X    X  XXXXX       X    X   X  XXXXX       X      X    XXXXX\n\n\n\n");   
 }
+
+
+
+/*
+void input()
+{
+	
+}
+*/
+
 
 
 void instruction(bool *isGameActiveP)
@@ -22,7 +33,6 @@ printf("Crosses go first.\n");
 printf("Player wins if he makes up the right combination.\n");
 printf("\nThis is the game field:\n");	
 int l=0;
-
 for(int i=0; i<3; i++){
 	for(int j=0; j<3; j++){
 		printf("| %c ", char(49+l));
@@ -31,7 +41,6 @@ for(int i=0; i<3; i++){
 	printf("|");
 	printf("\n");
 }
-
 printf("\nTo win you should cross numbers diagonally, vertically or horizontally.");
 printf("\n\nFor example, \"X\" player wins here:\n");
 l=0;
@@ -46,29 +55,38 @@ for(int i=0; i<3; i++){
 }
 		printf("\nIf you both can't cross entire field, game is draw.");
 		
-printf("\n\nPress any key to go to main menu");
+printf("\n\nPress any key to go to main menu...");
 getch();
 *isGameActiveP = true;
-
 }
-/*
-void input()
-{
-	
-}
-void gameField()
-{
-	
 
-*/
+
+
+void about(bool *isGameActiveP)
+{
+	system("cls");
+	printf("The game was developed for programming coursework.\n");
+	printf("\nCreated by Ilya Konnov.\n");
+	printf("For more information check out my github profile: \nhttps://github.com/konnov-007");	
+	
+	printf("\n\nPress any key to go to main menu...");
+	getch();
+*isGameActiveP = true;	
+}
+
+
+
 void gameMenu(bool *isGameActiveP)
 {	
 	system("cls");
 	title();
 	
 	printf("Welcome to \"Tic-Tac\" game.\n\n");
-	printf("1)Play\n2)Instruction\n3)Quit\n");
+	printf("1)Play\n2)Instruction\n3)About\n4)Quit\n");
 	unsigned char inputChar = _getch();
+	
+	 if((inputChar!='1')&&(inputChar!='2')&&(inputChar!='3')&&(inputChar!='4'))
+	    *isGameActiveP=true;
 	
 	switch (inputChar)
 	{
@@ -85,22 +103,30 @@ void gameMenu(bool *isGameActiveP)
 		    break;
 	    }
 
-	    case '3':
+		case '3':
+		{
+			about(isGameActiveP);
+			break;	
+		}
+		
+	    case '4':
 	    {
 	    	*isGameActiveP=false;
 		    break;
 	    }
+	   
 }
 }
+
 
 
 main()
 {
-	bool isGameActive;
+bool isGameActive;
 	
 	do{
 	gameMenu(&isGameActive);
-	//gameField();
 }while(isGameActive==true);
+
 	return 0;	
 }
