08e3dbf2fe9a801b2d2dc525315e6861e7a3e8d9
Mr.Konnov <konnov-007@mail.ru>
2016-04-18 11:09:36 +0600

    Fixed title

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@ char table[3][3];
 void title()
 {
 	printf("XXXXX  X  XXXXX     XXXXX    X    XXXXX     XXXXX    X    XXXXX\n");
-	printf("  X       X     XXX   X    X   X  X     XXX   X    X   X  X    \n");
-	printf("  X    X  X     XXX   X    XXXXX  X     XXX   X    X   X  X    \n");
-	printf("  X    X  XXXXX       X    X   X  XXXXX       X      X    XXXXX\n\n\n\n");   
+	printf("  X       X     XXX   X    X   X  X     XXX   X    X   X  X\n");
+	printf("  X    X  X     XXX   X    XXXXX  X     XXX   X    X   X  XXXXX\n");
+	printf("  X    X  X           X    X   X  X           X    X   X  X\n");   
+	printf("  X    X  XXXXX       X    X   X  XXXXX       X      X    XXXXX\n\n\n\n");
 }
 
 
