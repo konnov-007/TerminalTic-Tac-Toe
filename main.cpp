db519ad23034c9fdd75487c46e800d94b7ea02a8
Mr.Konnov <konnov-007@mail.ru>
2016-04-19 17:39:06 +0600

    Beta version

--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@ void title()
 void mainGame(bool *isGameActiveP)
 {
 	system("cls");
+	srand(time(NULL));
 int i = 0;
 int player = 0;
 int go = 0;
@@ -53,20 +54,46 @@ printf("-----------\n");
         printf(" %c | %c | %c\n", board[2][0], board[2][1], board[2][2]);
         
 player = i%2 + 1; 
+
+ do{
 if(player==1) printf("%s %c: ", message1,(player==1)?'X':'O');
 if(player==2) printf("%s ", message2);
 
- /*do{
-printf("\nPlayer %d, please enter the number of the square "
-                   "where you want to place your %c: ", player,(player==1)?'X':'O');
-            scanf("%d", &go);
-            
+if(player==1) scanf("%d", &go);
+if(player==2) go=rand()%9+1;
+
             row = --go/3;
             column = go%3;
         }
-        while(go<0 || go>9 || board[row][column]>'9');*/
-	
+        while(go<0 || go>9 || board[row][column]>'9');
+        
+         board[row][column] = (player == 1) ? 'X' : 'O';
+        
+        if((board[0][0] == board[1][1] && board[0][0] == board[2][2]) ||
+           (board[0][2] == board[1][1] && board[0][2] == board[2][0]))
+            winner = player;
+        else            
+            for(line = 0; line <= 2; line ++)
+                
+                if((board[line][0] == board[line][1] && board[line][0] == board[line][2])||
+                   (board[0][line] == board[1][line] && board[0][line] == board[2][line]))
+                    winner = player;
 }
+  printf("\n\n");
+    printf(" %c | %c | %c\n", board[0][0], board[0][1], board[0][2]);
+printf("-----------\n");
+    printf(" %c | %c | %c\n", board[1][0], board[1][1], board[1][2]);
+printf("-----------\n");
+    printf(" %c | %c | %c\n", board[2][0], board[2][1], board[2][2]);
+    
+    if(winner == 0)
+        printf("\nDraw\n");
+    else{
+		if(winner==1) printf("\nYou won!");
+    	if(winner==2) printf("\nComputer won!");
+}
+
+
 printf("\n\nPress any key to return to main menu...");
 getch();
 		*isGameActiveP=true;
